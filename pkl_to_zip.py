3a648ab20dda9152cd6f34fc9c6c52c5700187c9
Ivan <60277@mail.ru>
2020-12-02 01:27:14 +0500

    update pkl_to_zip

--- pkl_to_zip.py
+++ pkl_to_zip.py
@@ -5,10 +5,10 @@ import zipfile
 import shutil
 from project_tools.os_tools import generator_files_in_dir
 #%%
-project_dir = r"E:\!hh.ru\vacancies_pickle_all_2020"
+project_dir = r"F:\!hh.ru"
 
-src_dir = f"{project_dir}\src_dir"
-dst_dir = f"{project_dir}\dst_dir"
+src_dir = f"{project_dir}\\vacancies_pickle_all"
+dst_dir = f"{project_dir}\\vacancies_pickle_all_zip"
 
 if os.path.exists(dst_dir):
     shutil.rmtree(dst_dir)
@@ -19,13 +19,13 @@ files = [filename for filename in generator_files_in_dir(src_dir)]
 print(len(files))
 
 #%%
-for filename_pkl in tqdm(files):
+for filename_pkl in tqdm(files[:]):
     sub_dir = '\\'.join(filename_pkl.split('\\')[:-1]).replace(src_dir, dst_dir)
     print(sub_dir)
     if not os.path.exists(sub_dir):
         os.mkdir(sub_dir)
 
-    filename_zip = filename_pkl.replace(src_dir, dst_dir).replace('.pkl', '.zip')
+    filename_zip = filename_pkl.replace(src_dir, dst_dir).replace('.pickle', '.zip')
     if not os.path.exists(filename_zip):
         with zipfile.ZipFile(filename_zip, 'w', zipfile.ZIP_DEFLATED) as myzip:
             myzip.write(filename_pkl, os.path.basename(filename_pkl))
